f398cc4885654a26213ac8800908a5efcbaa5219
skydread1 <loic.blanchard01@gmail.com>
2022-05-25 17:00:17 +0800

    Use dark mode by default

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// Sets dark mode by default on first visit
+if (localStorage.theme === undefined){
+    localStorage.setItem('theme', 'dark');
+}
+
 root.render(
     <App />
 );
