38c5556d8e82978b7bd95b00eb61d76da3d7a868
skydread1 <loic.blanchard01@gmail.com>
2022-12-27 10:57:00 +0800

    Update test samples to add author to post

--- dev/src/clj/flybot/systems.clj
+++ dev/src/clj/flybot/systems.clj
@@ -4,7 +4,8 @@
             [clj.flybot.db :as db]
             [clojure.edn :as edn]
             [datalevin.core :as d]
-            [clj.flybot.sample-data2 :as sample-data]
+            [clj.flybot.dev-sample-data :as ds]
+            [cljc.flybot.test-sample-data :as ts]
             [robertluo.fun-map :refer [fnk closeable touch halt!]]))
 
 (defn system-config
@@ -12,17 +13,22 @@
   (let [env-cfg (-> (slurp "config/system.edn") edn/read-string env)]
     (merge env-cfg core/oauth2-config)))
 
-(def db-conn-system
+(defn db-conn-system
   "On touch: empty the db and get conn.
    On halt!: close conn and ampty the db."
+  [schemas]
   (fnk [db-uri]
        (let [conn (d/get-conn db-uri)
              _    (d/clear conn)
              conn (d/get-conn db-uri db/initial-datalevin-schema)]
-         (sample-data/add-initial-data conn)
+         @(d/transact conn schemas)
          (closeable
           {:conn conn}
           #(d/clear conn)))))
+
+(def dev-schemas (concat ds/home-posts ds/apply-posts
+                         ds/about-posts ds/blog-posts
+                         ds/pages))
 
 ;;---------- System for front-end dev ----------
 ;; Figwheel automatically start the system for us via the figwheel-main.edn on port 9500
@@ -32,7 +38,7 @@
 (def figwheel-system
   (-> (system-config :figwheel)
       core/system
-      (assoc :db-conn db-conn-system)
+      (assoc :db-conn (db-conn-system dev-schemas))
       (dissoc :http-port :http-server)))
 
 (def figwheel-handler
@@ -46,10 +52,19 @@
 
 (def dev-system
   (-> (core/system (system-config :dev))
-      (assoc :db-conn db-conn-system)))
+      (assoc :db-conn (db-conn-system dev-schemas))))
+
+;;---------- System for backend tests ----------
+;; It uses some dumy data
+
+(def test-schemas [ts/post-1 ts/post-2
+                   ts/home-page ts/apply-page
+                   ts/bob-user ts/alice-user])
+
 (comment
   (touch figwheel-system)
-  (halt! figwheel-system))
+  (halt! figwheel-system)
+  )
 
 (comment
   (touch dev-system)
