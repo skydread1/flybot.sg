d692d940d7faa3499eb8aad1bb47398ad8656f3a
skydread1 <loic.blanchard01@gmail.com>
2023-02-17 11:58:58 +0800

    Refactor namespaced maps

--- server/src/flybot/server/core/init_data.clj
+++ server/src/flybot/server/core/init_data.clj
@@ -14,113 +14,115 @@
       slurp))
 
 (def about-posts
-  [{:post/id (u/mk-uuid)
-    :post/page :about
-    :post/css-class "company"
-    :post/creation-date (u/mk-date)
-    :post/md-content (slurp-md "about" "company.md")
-    :post/image-beside {:image/src "assets/flybot-logo.png"
-                        :image/src-dark "assets/flybot-logo.png"
-                        :image/alt "Flybot Logo"}}
-   {:post/id (u/mk-uuid)
-    :post/page :about
-    :post/css-class "team"
-    :post/creation-date (u/mk-date)
-    :post/md-content (slurp-md "about" "team.md")}])
+  [#:post{:id (u/mk-uuid)
+          :page :about
+          :css-class "company"
+          :creation-date (u/mk-date)
+          :md-content (slurp-md "about" "company.md")
+          :image-beside #:image{:src "assets/flybot-logo.png"
+                                :src-dark "assets/flybot-logo.png"
+                                :alt "Flybot Logo"}}
+   #:post{:id (u/mk-uuid)
+          :page :about
+          :css-class "team"
+          :creation-date (u/mk-date)
+          :md-content (slurp-md "about" "team.md")}])
 
 (def apply-posts
-  [{:post/id (u/mk-uuid)
-    :post/page :apply
-    :post/css-class "description"
-    :post/creation-date (u/mk-date)
-    :post/md-content (slurp-md "apply" "description.md")}
-   {:post/id (u/mk-uuid)
-    :post/page :apply
-    :post/css-class "qualifications"
-    :post/creation-date (u/mk-date)
-    :post/md-content (slurp-md "apply" "qualifications.md")}
-   {:post/id (u/mk-uuid)
-    :post/page :apply
-    :post/css-class "goal"
-    :post/creation-date (u/mk-date)
-    :post/md-content (slurp-md "apply" "goal.md")}
-   {:post/id (u/mk-uuid)
-    :post/page :apply
-    :post/css-class "application"
-    :post/creation-date (u/mk-date)
-    :post/md-content (slurp-md "apply" "application.md")}])
+  [#:post{:id (u/mk-uuid)
+          :page :apply
+          :css-class "description"
+          :creation-date (u/mk-date)
+          :md-content (slurp-md "apply" "description.md")}
+   #:post{:id (u/mk-uuid)
+          :page :apply
+          :css-class "qualifications"
+          :creation-date (u/mk-date)
+          :md-content (slurp-md "apply" "qualifications.md")}
+   #:post{:id (u/mk-uuid)
+          :page :apply
+          :css-class "goal"
+          :creation-date (u/mk-date)
+          :md-content (slurp-md "apply" "goal.md")}
+   #:post{:id (u/mk-uuid)
+          :page :apply
+          :css-class "application"
+          :creation-date (u/mk-date)
+          :md-content (slurp-md "apply" "application.md")}])
 
 (def blog-posts
-  [{:post/id (u/mk-uuid)
-    :post/page :blog
-    :post/css-class "welcome"
-    :post/creation-date (u/mk-date)
-    :post/show-dates? true
-    :post/md-content (slurp-md "blog" "welcome.md")}
-   {:post/id (u/mk-uuid)
-    :post/page :blog
-    :post/css-class "md-example"
-    :post/creation-date (u/mk-date)
-    :post/show-dates? true
-    :post/md-content (slurp-md "blog" "mdsample.md")}])
+  [#:post{:id (u/mk-uuid)
+          :page :blog
+          :css-class "welcome"
+          :creation-date (u/mk-date)
+          :show-dates? true
+          :md-content (slurp-md "blog" "welcome.md")}
+   #:post{:id (u/mk-uuid)
+          :page :blog
+          :css-class "md-example"
+          :creation-date (u/mk-date)
+          :show-dates? true
+          :md-content (slurp-md "blog" "mdsample.md")}])
 
 (def home-posts
-  [{:post/id (u/mk-uuid)
-    :post/page :home
-    :post/css-class "clojure"
-    :post/creation-date (u/mk-date)
-    :post/md-content (slurp-md "home" "clojure.md")
-    :post/image-beside {:image/src "assets/clojure-logo.svg"
-                        :image/src-dark "assets/clojure-logo-dark-mode.svg"
-                        :image/alt "Clojure Logo"}}
-   {:post/id (u/mk-uuid)
-    :post/page :home
-    :post/css-class "paradigms"
-    :post/creation-date (u/mk-date)
-    :post/md-content (slurp-md "home" "paradigms.md")
-    :post/image-beside {:image/src "assets/lambda-logo.svg"
-                        :image/src-dark "assets/lambda-logo-dark-mode.svg"
-                        :image/alt "Lambda Logo"}}
-   {:post/id (u/mk-uuid)
-    :post/page :home
-    :post/css-class "golden-island"
-    :post/creation-date (u/mk-date)
-    :post/md-content (slurp-md "home" "golden-island.md")
-    :post/image-beside {:image/src "assets/4suits.svg"
-                        :image/src-dark "assets/4suits-dark-mode.svg"
-                        :image/alt "4 suits of a deck"}}
-   {:post/id (u/mk-uuid)
-    :post/page :home
-    :post/css-class "magic"
-    :post/creation-date (u/mk-date)
-    :post/md-content (slurp-md "home" "magic.md")
-    :post/image-beside {:image/src "assets/binary.svg"
-                        :image/src-dark "assets/binary-dark-mode.svg"
-                        :image/alt "Love word written in base 2"}}])
+  [#:post{:id (u/mk-uuid)
+          :page :home
+          :css-class "clojure"
+          :creation-date (u/mk-date)
+          :md-content (slurp-md "home" "clojure.md")
+          :image-beside #:image{:src "assets/clojure-logo.svg"
+                                :src-dark "assets/clojure-logo-dark-mode.svg"
+                                :alt "Clojure Logo"}}
+   #:post{:id (u/mk-uuid)
+          :page :home
+          :css-class "paradigms"
+          :creation-date (u/mk-date)
+          :md-content (slurp-md "home" "paradigms.md")
+          :image-beside #:image{:src "assets/lambda-logo.svg"
+                                :src-dark "assets/lambda-logo-dark-mode.svg"
+                                :alt "Lambda Logo"}}
+   #:post{:id (u/mk-uuid)
+          :page :home
+          :css-class "golden-island"
+          :creation-date (u/mk-date)
+          :md-content (slurp-md "home" "golden-island.md")
+          :image-beside #:image{:src "assets/4suits.svg"
+                                :src-dark "assets/4suits-dark-mode.svg"
+                                :alt "4 suits of a deck"}}
+   #:post{:id (u/mk-uuid)
+          :page :home
+          :css-class "magic"
+          :creation-date (u/mk-date)
+          :md-content (slurp-md "home" "magic.md")
+          :image-beside #:image{:src "assets/binary.svg"
+                                :src-dark "assets/binary-dark-mode.svg"
+                                :alt "Love word written in base 2"}}])
 
 (def posts
   (concat home-posts apply-posts about-posts blog-posts))
 
 (def pages
-  [{:page/name :home
-    :page/sorting-method {:sort/type :post/creation-date
-                          :sort/direction :ascending}}
-   {:page/name :apply
-    :page/sorting-method {:sort/type :post/creation-date
-                          :sort/direction :ascending}}
-   {:page/name :about
-    :page/sorting-method {:sort/type :post/creation-date
-                          :sort/direction :ascending}}
-   {:page/name :blog
-    :page/sorting-method {:sort/type :post/creation-date
-                          :sort/direction :ascending}}])
+  [#:page{:name :home
+          :sorting-method #:sort{:type :creation-date
+                                 :direction :ascending}}
+   #:page{:name :apply
+          :sorting-method #:sort{:type :creation-date
+                                 :direction :ascending}}
+   #:page{:name :about
+          :sorting-method #:sort{:type :creation-date
+                                 :direction :ascending}}
+   #:page{:name :blog
+          :sorting-method #:sort{:type :creation-date
+                                 :direction :ascending}}])
 
 (def users
   (let [admin (edn/read-string (or (System/getenv "ADMIN_USER")
                                    (slurp "config/admin.edn")))]
     (-> admin
-        (assoc :user/roles [{:role/name :editor :role/date-granted (u/mk-date)}
-                            {:role/name :admin :role/date-granted (u/mk-date)}])
+        (assoc :user/roles [#:role{:name :editor
+                                   :date-granted (u/mk-date)}
+                            #:role{:name :admin
+                                   :date-granted (u/mk-date)}])
         vector)))
 
 (def init-data
