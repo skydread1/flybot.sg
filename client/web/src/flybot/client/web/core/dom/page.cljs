b940d6647e64444bf763cc3b77a5649ca20699bf
Nguyen Hoai Nam <nam_nguyenhoai@basecity.com>
2023-06-26 17:16:36 +0800

    Add unique UUID-based links for blog posts
    Individual blog posts can now be accessed through `/blog/<uuid>`, where
    `<uuid>` is the unique identifier of the post.

--- client/web/src/flybot/client/web/core/dom/page.cljs
+++ client/web/src/flybot/client/web/core/dom/page.cljs
@@ -5,12 +5,19 @@
             [flybot.client.web.core.dom.page.post :refer [page-post]]
             [re-frame.core :as rf]))
 
+(defn add-hiccup-content
+  [{:post/keys [md-content] :as post}]
+  (assoc post :post/hiccup-content (h/md->hiccup md-content)))
+
 (defn page
   "Given the `page-name`, returns the page content."
   [page-name]
-  (let [sorting-method @(rf/subscribe [:subs/pattern {:app/pages {page-name '{:page/sorting-method ?x}}}])
+  (let [sorting-method @(rf/subscribe [:subs/pattern
+                                       {:app/pages
+                                        {page-name
+                                         {:page/sorting-method '?x}}}])
         ordered-posts (->> @(rf/subscribe [:subs.post/posts page-name])
-                           (map #(assoc % :post/hiccup-content (h/md->hiccup (:post/md-content %))))
+                           (map add-hiccup-content)
                            (utils/sort-posts sorting-method))
         new-post      {:post/id "new-post-temp-id"}
         posts         (conj ordered-posts new-post)]
@@ -21,3 +28,26 @@
      (doall
       (for [post posts]
         (page-post page-name post)))]))
+
+(defn blog-post-page
+  "Given the blog post identifier, returns the corresponding post in a page."
+  []
+  (let [query-id @(rf/subscribe [:subs/pattern
+                                 {:app/current-view
+                                  {:parameters
+                                   {:path
+                                    {:id '?x}}}}])
+        all-blog-posts (->> @(rf/subscribe [:subs.post/posts :blog])
+                            (map add-hiccup-content))
+        queried-post (filter (fn [post]
+                               (= query-id (str (:post/id post))))
+                             all-blog-posts)
+        new-post      {:post/id "new-post-temp-id"}
+        posts         (conj queried-post new-post)]
+    [:section.container
+     {:class (name :blog)
+      :key   (name :blog)}
+     [page-header :blog]
+     (doall
+      (for [post posts]
+        (page-post :blog post)))]))
