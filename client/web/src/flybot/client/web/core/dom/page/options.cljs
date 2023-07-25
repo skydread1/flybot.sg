90b7fb056e0cbcfdb48ddcc0fb30cfce0bf55d26
flybot-nam-nguyenhoai <137136212+flybot-nam-nguyenhoai@users.noreply.github.com>
2023-07-25 13:39:23 +0800

    Add blog post sorting options for visitors (#201)
    * Add form for changing blog post sorting options
    
    * Fix sorting by post title
    
    * Refactor code
    
    * Remove sorting form from non-blog pages
    
    * Fix new post button
    
    * Hide database details in form HTML
    
    * Refactor code; fix form; restore new post button
    
    * Move form dispatch event to common namespace

---
+++ client/web/src/flybot/client/web/core/dom/page/options.cljs
@@ -0,0 +1,32 @@
+(ns flybot.client.web.core.dom.page.options
+  (:require [re-frame.core :as rf]))
+
+(defn blog-sorting-form
+  []
+  [:form
+   [:fieldset
+    [:legend "Sorting Options"]
+    [:label {:for "blog-sorting-options"} "Sort posts by:"]
+    [:select#blog-sorting-options
+     {:name "blog-sorting-options"
+      :value "[:date-created :descending]"
+      :on-change #(rf/dispatch [:evt.page.form/set-blog-sorting-options
+                                (.. % -target -value)])}
+     [:option
+      {:value "[:date-created :descending]"}
+      "Date created (newest first)"]
+     [:option
+      {:value "[:date-created :ascending]"}
+      "Date created (oldest first)"]
+     [:option
+      {:value "[:date-updated :descending]"}
+      "Date updated (newest first)"]
+     [:option
+      {:value "[:date-updated :ascending]"}
+      "Date updated (oldest first)"]
+     [:option
+      {:value "[:title :ascending]"}
+      "Title (A–Z)"]
+     [:option
+      {:value "[:title :descending]"}
+      "Title (Z–A)"]]]])
