92d08af718b98d207284da1b76394ce85ce7fa61
Nguyen Hoai Nam <nam_nguyenhoai@basecity.com>
2023-08-04 11:25:48 +0800

    Highlight blocks without languages as plain text

--- client/web/src/flybot/client/web/core/dom/hiccup.cljs
+++ client/web/src/flybot/client/web/core/dom/hiccup.cljs
@@ -3,24 +3,49 @@
             [markdown-to-hiccup.core :as mth]
             [re-frame.core :as rf]))
 
+(defn- hiccup-with-properties
+  "Add an empty property map to Hiccup vectors without one."
+  [[tag & rest :as hiccup]]
+  (if (map? (first rest))
+    hiccup
+    (into [tag {}] rest)))
+
 ;; ---------- Post hiccup conversion logic ----------
 
 (defn md-dark-image
-  "Extract the dark mode src from the markdown
-   and add it to the hiccup props."
+  "Extracts the dark mode src from the markdown
+  and add it to the hiccup props."
   [[tag {:keys [srcdark] :as props} value]] 
   (if (and srcdark (= :dark @(rf/subscribe [:subs/pattern '{:app/theme ?x}])))
     [tag (assoc props :src (:srcdark props)) value]
     [tag props value]))
 
+(defn md-code-block-default-plaintext
+  "Marks code blocks without any specified languages as plain text."
+  [h]
+  (let [[_ props content] (hiccup-with-properties h)]
+    (if (= :code (get content 0))
+      (let [[_ code-props code-content] (hiccup-with-properties content)]
+        [:pre props
+         [:code (merge {:class "text"} code-props)
+          code-content]])
+      h)))
+
 (defn post-hiccup
-  "Given the hiccup, apply some customisation."
+  "Given the Hiccup content, applies customizations to images and code blocks:
+
+  - Add dark versions to images.
+  - Mark code blocks without any specified languages as plain text."
   [hiccup]
   (->> hiccup
        (postwalk
         (fn [h]
-          (if (and (vector? h) (= :img (first h)))
+          (cond
+            (and (vector? h) (= :img (first h)))
             (md-dark-image h)
+            (and (vector? h) (= :pre (first h)))
+            (md-code-block-default-plaintext h)
+            :else
             h)))))
 
 ;; ---------- Markdown to Hiccup ----------
